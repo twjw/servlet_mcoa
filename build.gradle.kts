ff09886e37598a223df01cb5952ffc78437bbeca
twjw <aaaawjwa@gmail.com>
2024-03-12 23:10:43 +0800

    配置基本的 servlet 結構

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,11 +5,18 @@ plugins {
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8 // 指定支持的 Java 代碼語言級別
+    targetCompatibility = JavaVersion.VERSION_1_8 // 設置用於確定應該生成哪個Java版本的.class字節碼
+}
+
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    compileOnly("javax.servlet:javax.servlet-api:3.1.0")
+
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
--- readme.md
+++ readme.md
@@ -1,7 +1,8 @@
 servlet_mcoa
 ===
 
-mysql 8 (utf8mb4, utf8mb4_general_ci)  
+tomcat 8  
+mysql 8 (utf8mb4, utf8mb4_general_ci, docker run --name mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=123456 -d mysql:8)  
 mybatis 3.5  
 alibaba druid  
-servlet 3.1
+servlet 3.1  
